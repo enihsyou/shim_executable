dd38450a203d9cfd9977581a2f6bef13b19e1028
John P. Hilbert <jphilbert@gmail.com>
2025-02-09 06:02:49 -0500

    Update get_argument.h

--- get_argument.h
+++ get_argument.h
@@ -1,29 +1,35 @@
+// ------------------------------------------------------------------------- //
+// Get Command Line Methods                                                  // 
+// ------------------------------------------------------------------------- //
 /**@file    GET_ARGUMENTS.H
  * @brief   Provides functions to parse and get command line arguments
  * @author  John P. Hilbert
  * @email   jphilbert@gmail.com
  * @date    1/18/2025
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * ------------------------------------------------------------------------- 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <https://www.gnu.org/licenses/>.
+ * ------------------------------------------------------------------------- */
 
 #ifndef GET_ARGUMENTS_H
 #define GET_ARGUMENTS_H
 
 // ------------------------------------------------------------------------- //
+#include <windows.h>
 #include <string>
 #include <regex>
+#include <vector>
 
 #define QUOTE_REGEX     L"((?:^|[^\\\\])(?:\\\\{2})*)\""
 #define WORD_REGEX      L"[^\\s=]+"
